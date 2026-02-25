ed36d0397f34d39572194ed13e446e6e4533f41c
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-02-25 19:34:11 +0100

    Modularización inicial y primera parte de la función del botón de cambio de tema

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,88 @@
 "use strict";
 
-/* Creación de botón hamburguesa interactivo */
+/* =======================================================
+   BOTÓN HAMBURGUESA
+   ======================================================= */
 
-const botonHamburguesa = document.querySelector(".cabecera__buscador--hamburguesa")
-const cabecera = document.querySelector('header');
+const cabecera = document.querySelector('header')
+function interactividadHamburguesa(cabecera){
+    const botonHamburguesa = document.querySelector(".cabecera__buscador--hamburguesa")
 
-const menuNavegacion = document.createElement('nav')
-menuNavegacion.classList.add('hamburguesa-nav');
+    const menuNavegacion = document.createElement('nav')
+    menuNavegacion.classList.add('hamburguesa-nav')
 
-const navegacionLista = document.createElement('ul')
-menuNavegacion.append(navegacionLista)
+    const navegacionLista = document.createElement('ul')
+    menuNavegacion.append(navegacionLista)
 
-const enlacesNavegacion = document.querySelectorAll('header ul li a')
+    const enlacesNavegacion = document.querySelectorAll('header ul li a')
 
-for(let i = 0; i < enlacesNavegacion.length; i++){
-    const apartado = document.createElement('li')
-    const enlace = document.createElement('a')
+    for(let i = 0; i < enlacesNavegacion.length; i++){
+        const apartado = document.createElement('li')
+        const enlace = document.createElement('a')
 
-    enlace.textContent = enlacesNavegacion[i].textContent
-    enlace.href = enlacesNavegacion[i].href
+        enlace.textContent = enlacesNavegacion[i].textContent
+        enlace.href = enlacesNavegacion[i].href
 
-    apartado.append(enlace)
-    navegacionLista.append(apartado)
-}
-    if(!document.querySelector('.introduccion')){
-        const apartadoInicio = document.createElement('li')
-        const enlaceInicio = document.createElement('a')
-
-        enlaceInicio.textContent = 'Inicio'
-        enlaceInicio.href = document.querySelector('.cabecera__logo').href
-
-        apartadoInicio.append(enlaceInicio)
-        navegacionLista.prepend(apartadoInicio)
+        apartado.append(enlace)
+        navegacionLista.append(apartado)
     }
+        if(!document.querySelector('.introduccion')){
+            const apartadoInicio = document.createElement('li')
+            const enlaceInicio = document.createElement('a')
 
-botonHamburguesa.after(menuNavegacion)
+            enlaceInicio.textContent = 'Inicio'
+            enlaceInicio.href = document.querySelector('.cabecera__logo').href
 
-botonHamburguesa.addEventListener("click",() => {
-    menuNavegacion.classList.toggle('hamburguesa-nav__activa')
-    const siguienteSeccion = cabecera.nextElementSibling;
-    const piePagina = document.querySelector('footer');
-    siguienteSeccion.style.transition = "transform 0.4s ease"
-    piePagina.style.transition = "transform 0.4s ease"
-    if (menuNavegacion.classList.contains('hamburguesa-nav__activa')) {
-        const alturaMenu = menuNavegacion.scrollHeight;
-        siguienteSeccion.style.transform = `translateY(${alturaMenu}px)`;
-        piePagina.style.transform = `translateY(${alturaMenu}px)`;
+            apartadoInicio.append(enlaceInicio)
+            navegacionLista.prepend(apartadoInicio)
+        }
+
+    botonHamburguesa.after(menuNavegacion)
+
+    botonHamburguesa.addEventListener("click",() => {
+        menuNavegacion.classList.toggle('hamburguesa-nav__activa')
+        const siguienteSeccion = cabecera.nextElementSibling
+        const piePagina = document.querySelector('footer')
+        siguienteSeccion.style.transition = "transform 0.4s ease"
+        piePagina.style.transition = "transform 0.4s ease"
+        if (menuNavegacion.classList.contains('hamburguesa-nav__activa')) {
+            const alturaMenu = menuNavegacion.scrollHeight
+            siguienteSeccion.style.transform = `translateY(${alturaMenu}px)`
+            piePagina.style.transform = `translateY(${alturaMenu}px)`
+        }
+        else{
+            siguienteSeccion.style.transform = ""
+        }
+    })
+}
+
+/* =======================================================
+   MODO OSCURO
+   ======================================================= */
+function modoOscuro(cabecera){
+    const logo = document.querySelector('.cabecera__logo')
+    const boton = document.createElement('button')
+    logo.replaceWith(boton)
+
+    const temaGuardado = localStorage.getItem('tema-nintendo')
+    const prefiereOscuro = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+    if(temaGuardado === 'oscuro' || (!temaGuardado && prefiereOscuro)){
+        boton.classList.add('tema-oscuro')
+        botonTema.textContent = '☀️';
     }
     else{
-        siguienteSeccion.style.transform = ""
+        boton.classList.add('tema-claro')
+        botonTema.textContent = '🌙';
     }
-})
+
+    boton.addEventListener('click', () =>{
+        if(document.body.classList.contains('tema-oscuro')){
+            body.classList.remove('tema-oscuro')
+        }
+    })
+}
+
+
+
+interactividadHamburguesa(cabecera)
